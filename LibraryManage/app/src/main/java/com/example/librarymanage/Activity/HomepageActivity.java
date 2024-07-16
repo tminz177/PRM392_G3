4de0c2d5682d65775e7e0d280773445c578be411
tminz177 <pt.minz177@gmail.com>
2024-07-16 09:44:51 +0700

    ...

--- LibraryManage/app/src/main/java/com/example/librarymanage/Activity/HomepageActivity.java
+++ LibraryManage/app/src/main/java/com/example/librarymanage/Activity/HomepageActivity.java
@@ -1,9 +1,7 @@
 package com.example.librarymanage.Activity;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
@@ -12,7 +10,8 @@ import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.librarymanage.Adapter.BookAdapter;
-import com.example.librarymanage.DTO.BookDTO;
+import com.example.librarymanage.DAO.AuthorDAO;
+import com.example.librarymanage.DAO.BookDAO;
 import com.example.librarymanage.DTO.RoomDB;
 import com.example.librarymanage.Entity.*;
 import com.example.librarymanage.R;
@@ -21,31 +20,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HomepageActivity extends AppCompatActivity {
-    private ProgressDialog progressDialog;
-    private List<Book> book = new ArrayList<>();
     private BookAdapter bookAdapter;
     private RecyclerView recyclerView;
+    private RoomDB roomDB;
+    private BookDAO bookDao;
+    private AuthorDAO authorDao;
 
     RoomDB database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.homepage);
-        ImageSlider imageSlider;
-        imageSlider = findViewById(R.id.image_slider);
-        ArrayList<SlideModel> imageList = new ArrayList<>();
 
-        imageList.add(new SlideModel(R.drawable.ic_launcher_background, ScaleTypes.FIT));
+        loadImageSlider();
+        getRoomData();
+    }
 
-        imageSlider.setImageList(imageList);
+    public void getRoomData(){
+        roomDB = RoomDB.getInstance(this);
+        bookDao = roomDB.bookDAO();
+        authorDao = roomDB.authorDAO();
 
         recyclerView = findViewById(R.id.bookRecyclerView);
         recyclerView.setLayoutManager(
                 new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         );
-        List<BookDTO> book = new ArrayList<>();
-        recyclerView.setAdapter(new BookAdapter(HomepageActivity.this, book));
+
+        List<Book> bookList = bookDao.getBookList();
+        List<Author> authorList = authorDao.getAuthorList();
+
+        recyclerView.setAdapter(new BookAdapter(HomepageActivity.this, bookList, authorList));
+    }
+
+    public void loadImageSlider(){
+        ImageSlider imageSlider = findViewById(R.id.image_slider);
+        ArrayList<SlideModel> imageList = new ArrayList<>();
+
+        imageList.add(new SlideModel(R.drawable.ic_launcher_background, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.side_nav_bar, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.search_background, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.ic_launcher_background, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.ic_launcher_background, ScaleTypes.FIT));
+
+        imageSlider.setImageList(imageList, ScaleTypes.FIT);
     }
 
 }
